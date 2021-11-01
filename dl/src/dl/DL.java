b02141032e070bed4adb58d3dcce6699965f2076
Octacilio C. de Almeida <octa.oca44@gmail.com>
2021-11-01 17:54:50 -0300

    classe principal

--- dl/src/dl/DL.java
+++ dl/src/dl/DL.java
@@ -1,12 +1,24 @@
 package dl;
 
+import lexer.Lexer;
 import lexer.Tag;
 import lexer.Token;
 
+import java.io.File;
+
 public class DL {
     public static void main(String[] args){
-        Token t1 = new Token(Tag.ASSING, "=");
-        Token t2 = new Token(Tag.LE, "<=");
-        System.out.println("token1: "+t1+ " - ttoken2: "+t2);
+ //      Token t1 = new Token(Tag.ASSING, "=");
+//        Token t2 = new Token(Tag.LE, "<=");
+//        System.out.println("token1: "+t1+ " - ttoken2: "+t2);
+
+        Lexer l = new Lexer(new File("prog.dl"));
+        Token t = l.nextToken();
+        while (t.getTag() != Tag.EOF) {
+            System.out.println(t);
+            t = l.nextToken();
+        }
+
+        //continuar do exercicio
     }
 }
